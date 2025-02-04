8faad9bd689b5d05e7c6d5636d7378074779ef29
yaaqoub <yaaqoubcourses@gmail.com>
2025-02-04 08:40:38 +0100

    finished_mandatory

---
+++ ft_printf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ycharkou <ycharkou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 12:56:40 by ycharkou          #+#    #+#             */
+/*   Updated: 2025/01/30 15:43:06 by ycharkou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
+
+int		ft_printf(const char *format, ...);
+int		ft_putchar_fd(char c, int fd);
+int		ft_putstr_fd(char *s, int fd);
+int		ft_nbr(unsigned long n, char *base);
+
+#endif
